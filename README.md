0877a7dd120012b8b6d23c1722fd929822fb58cb
Chris Howe-Jones <chris.howejones@gmail.com>
2016-12-16 12:50:23 +0000

    Added figwheel

--- README.md
+++ README.md
@@ -21,9 +21,67 @@ Once you have these installed you can build and run the app using:
 
 
 
-## Options
+## Development
 
-FIXME: listing of options this app accepts.
+### Using Figwheel
+
+You can run Figwheel either from the REPL or from 'lein figwheel'. The
+recommended approach is via the REPL.
+
+##### Using CIDER in Emacs
+
+Start a CIDER nREPL server and two nREPL buffers (one for Clojure and
+one for ClojureScript) by using:
+
+``` emacs-lisp
+C-c M-J
+```
+*Note* the capital 'J'
+
+or:
+
+``` emacs-lisp
+M-x cider-jack-in-clojurescript
+```
+
+Once the buffers are started you can switch to the CLJS buffer and
+exit the Rhino cljs repl and connect to the app build running in the
+browser (make sure you run node and connect a browser):
+
+``` clojure
+cljs.user> :cljs/quit
+user> (use 'figwheel-sidecar.repl-api)
+nil
+user> (start-figwheel!)
+... _starts app build by default as it's first in project.clj_
+...
+nil
+user> (cljs-repl)
+Launching ClojureScript REPL for build: app ...
+nil
+cljs.user>
+```
+
+This CLJS buffer will now be connected to the browser. To connect a
+buffer to the server as well.
+
+``` emacs-lisp
+M-x cider-connect
+localhost
+<port for nREPL server - see nrepl-server buffer
+```
+
+``` clojure
+user> (use 'figwheel-sidecar.repl-api)
+nil
+user> (switch-to-build "server")
+user> (cljs-repl)
+Launching ClojureScript REPL for build: server...
+nil
+cljs.user>
+```
+
+This new buffer is now connected to the server running in node.
 
 ## Examples
 
--- project.clj
+++ project.clj
@@ -10,25 +10,34 @@
                  [secretary "1.2.3"]
                  [kibu/pushy "0.3.6"]]
 
-  :plugins [[lein-cljsbuild "1.1.5"]]
+  :plugins [[lein-cljsbuild "1.1.5"]
+            [lein-figwheel "0.5.4-7"]]
 
 
   :clean-targets ^{:protect false} ["resources"]
 
-:cljsbuild {
-    :builds [{:id "server"
-              :source-paths ["src" "src-server"]
-              :compiler {
-                :main isomorphic-node.server
-                :output-to "resources/public/js/server-side/server.js"
-                :output-dir "resources/public/js/server-side"
-                :target :nodejs
-                :optimizations :none
-                :source-map true}}
-             {:id "app"
-              :source-paths ["src" "src-client"]
-              :compiler {
-                :output-to "resources/public/js/app.js"
-                :output-dir "resources/public/js"
-                :optimizations :none
-                :source-map true}}]})
+  :profiles {:dev
+             {:dependencies [[com.cemerick/piggieback "0.2.1"]
+                             [org.clojure/core.async "0.2.395"]
+                             [figwheel-sidecar "0.5.4-7"
+                              :exclusions [org.clojure/core.async]]]
+              :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
+
+  :cljsbuild {:builds [{:id "app"
+                        :figwheel true
+                        :source-paths ["src" "src-client"]
+                        :compiler {
+                                   :output-to "resources/public/js/app.js"
+                                   :output-dir "resources/public/js"
+                                   :optimizations :none
+                                   :source-map true}}
+                       {:id "server"
+                        :figwheel true
+                        :source-paths ["src" "src-server"]
+                        :compiler {
+                                   :main isomorphic-node.server
+                                   :output-to "resources/public/js/server-side/server.js"
+                                   :output-dir "resources/public/js/server-side"
+                                   :target :nodejs
+                                   :optimizations :none
+                                   :source-map true}}]})
